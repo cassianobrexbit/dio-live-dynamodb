3558c813f56a32022c3242ff69dd9d4b5dfde499
Dyego Henrique <dyego_henrique-boo@hotmail.com>
2023-05-13 01:59:18 -0300

    Fix: --global-secondary-indexes.

--- dbConfig.sh
+++ dbConfig.sh
@@ -15,70 +15,69 @@ aws dynamodb create-table \
     --attribute-definitions \
         AttributeName=Artist,AttributeType=S \
         AttributeName=SongTitle,AttributeType=S \
+        AttributeName=AlbumTitle,AttributeType=S \
+        AttributeName=SongYear,AttributeType=S \
     --key-schema \
         AttributeName=Artist,KeyType=HASH \
         AttributeName=SongTitle,KeyType=RANGE \
     --provisioned-throughput \
-        ReadCapacityUnits=10,WriteCapacityUnits=5
-
-# Aguarda a entrada do usuário
-
-read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
-
-# Inserir um item
-
-aws dynamodb put-item \
-    --table-name Music \
-    --item file://./src/itemmusic.json
-
-# Aguarda a entrada do usuário
-
-read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
-
-# Inserir múltiplos itens
-
-aws dynamodb batch-write-item \
-    --request-items file://./src/batchmusic.json
-
-# Aguarda a entrada do usuário
-
-read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
-
-# Criar um index global secundário baeado no título do álbum
-
-aws dynamodb update-table \
-    --table-name Music \
-    --attribute-definitions AttributeName=AlbumTitle,AttributeType=S \
-    --global-secondary-index-updates \
-        "[{\"Create\":{\"IndexName\": \"AlbumTitle-index\",\"KeySchema\":[{\"AttributeName\":\"AlbumTitle\",\"KeyType\":\"HASH\"}], \
-        \"ProvisionedThroughput\": {\"ReadCapacityUnits\": 10, \"WriteCapacityUnits\": 5},\"Projection\":{\"ProjectionType\":\"ALL\"}}}]"
-
-# Aguarda a entrada do usuário
-
-read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
-
-# Criar um index global secundário baseado no nome do artista e no título do álbum
-
-aws dynamodb update-table \
-    --table-name Music \
-    --attribute-definitions\
-        AttributeName=Artist,AttributeType=S \
-        AttributeName=AlbumTitle,AttributeType=S \
-    --global-secondary-index-updates \
-        "[{\"Create\":{\"IndexName\": \"ArtistAlbumTitle-index\",\"KeySchema\":[{\"AttributeName\":\"Artist\",\"KeyType\":\"HASH\"}, {\"AttributeName\":\"AlbumTitle\",\"KeyType\":\"RANGE\"}], \
-        \"ProvisionedThroughput\": {\"ReadCapacityUnits\": 10, \"WriteCapacityUnits\": 5      },\"Projection\":{\"ProjectionType\":\"ALL\"}}}]"
-
-# Aguarda a entrada do usuário
-
-read -n 1 -s -r -p "Pressione qualquer tecla para continuar..."
-
-# Criar um index global secundário baseado no título da música e no ano
-
-aws dynamodb update-table \
-    --table-name Music \
-    --attribute-definitions\
-        AttributeName=SongTitle,AttributeType=S \
-        AttributeName=SongYear,AttributeType=S \
-    --global-secondary-index-updates \
-        "[{\"Create\":{\"IndexName\": \"SongTitleYear-index\",\"KeySchema\":[{\"AttributeName\":\"SongTitle\",\"KeyType\":\"HASH\"}, {\"AttributeName\":\"SongYear\",\"KeyType\":\"RANGE\"}], \
-        \"ProvisionedThroughput\": {\"ReadCapacityUnits\": 10, \"WriteCapacityUnits\": 5      },\"Projection\":{\"ProjectionType\":\"ALL\"}}}]"
+        ReadCapacityUnits=10,WriteCapacityUnits=5 \
+    --global-secondary-indexes \
+        "[
+            {
+                \"IndexName\":\"AlbumTitle-index\",
+                \"KeySchema\":[
+                    {
+                        \"AttributeName\":\"AlbumTitle\",
+                        \"KeyType\":\"HASH\"
+                    }
+                ],
+                \"ProvisionedThroughput\":{
+                    \"ReadCapacityUnits\":10,
+                    \"WriteCapacityUnits\":5
+                },
+                \"Projection\":{
+                    \"ProjectionType\":\"ALL\"
+                }
+            },
+            {
+                \"IndexName\":\"ArtistAlbumTitle-index\",
+                \"KeySchema\":[
+                    {
+                        \"AttributeName\":\"Artist\",
+                        \"KeyType\":\"HASH\"
+                    },
+                    {
+                        \"AttributeName\":\"AlbumTitle\",
+                        \"KeyType\":\"RANGE\"
+                    }
+                ],
+                \"ProvisionedThroughput\":{
+                    \"ReadCapacityUnits\":10,
+                    \"WriteCapacityUnits\":5
+                },
+                \"Projection\":{
+                    \"ProjectionType\":\"ALL\"
+                }
+            },
+            {
+                \"IndexName\":\"SongTitleYear-index\",
+                \"KeySchema\":[
+                    {
+                        \"AttributeName\":\"SongTitle\",
+                        \"KeyType\":\"HASH\"
+                    },
+                    {
+                        \"AttributeName\":\"SongYear\",
+                        \"KeyType\":\"RANGE\"
+                    }
+                ],
+                \"ProvisionedThroughput\":{
+                    \"ReadCapacityUnits\":10,
+                    \"WriteCapacityUnits\":5
+                },
+                \"Projection\":{
+                    \"ProjectionType\":\"ALL\"
+                }
+            }
+        ]"
